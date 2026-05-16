d645071462d5e3ae2f674c0ae582cfe5d1ac8a67
Gautam Pappu <gautampappu@utexas.edu>
2026-05-16 14:32:02 -0400

    Initial Commit

---
+++ meetup-ios/meetup_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetup_iosApp.swift
+//  meetup-ios
+//
+//  Created by Gautam Pappu on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct meetup_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
